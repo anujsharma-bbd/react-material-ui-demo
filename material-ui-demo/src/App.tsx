36c9ccc7083ff680fd725a8a9a795443fb2680af
Anuj Sharma <anuj.sharma01@nagarro.com>
2023-02-14 18:29:02 +0530

    updates

--- material-ui-demo/src/App.tsx
+++ material-ui-demo/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './App.scss';
+import { Buttons } from './buttons';
 
 export const App = () => {
   return (
@@ -8,6 +9,9 @@ export const App = () => {
       <header className="App-header">
         React Demo Site with Material-UI V5
       </header>
+      <div>
+        <Buttons></Buttons>
+      </div>
     </div>
   );
 }
