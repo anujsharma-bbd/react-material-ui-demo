962d2563b6e13e5c3fa5fe2664d9ff5094ea1018
Anuj Sharma <anuj.sharma01@nagarro.com>
2023-02-14 17:33:46 +0530

    initial commit

---
+++ material-ui-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React Demo Site with Material-UI V5
+      </header>
+    </div>
+  );
+}
